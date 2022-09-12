30a334657aa0adc2f504bb5e3dd68918b88d7977
melaksenay <m.g.senay@wustl.edu>
2022-09-11 20:58:07 -0500

    More frontend changes

--- datavant-genetics.py
+++ datavant-genetics.py
@@ -1,19 +1,13 @@
-vcf_file = "/Users/loganfarrow/Desktop/gnomad.exomes.r2.1.1.sites.7.vcf.bgz"
-def read_vcf(vcf_file):
-    """
-    Read a vcf file to a dict of lists.
+from django.shortcuts import render
 
-    :param str vcf_file: Path to a vcf file.
-    :return: dict of lists of vcf records
-    :rtype: dict
-    """
-    vcf_dict = []
-    with open(vcf_file, 'r') as invcf:
-        for line in invcf:
-            if line.startswith('#'):
-                continue
-            line = line.strip().split()
-            vcf_dict.append((line[0], line[1], line[3], line[4]))
-    return vcf_dict 
-printer = read_vcf(vcf_file)
-print(printer)
+import requests
+
+def button(request):
+    return render(request,'contact.html')
+
+def output(request):
+    return "testingnn"
+
+def external(request):
+    inp=request.POST.get('param')
+
